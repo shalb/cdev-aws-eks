171bd24ce32398efe5a457149fb211a13e5aacfc
Volodymyr Tsap <voa@shalb.com>
2023-08-15 10:19:13 +0000

    Add Error Handling for Github operations

--- .cdev-metadata/streamlit/interface.py
+++ .cdev-metadata/streamlit/interface.py
@@ -65,7 +65,7 @@ def generate_stack_eks_yaml(project_yaml):
     cluster_name = st.text_input("Cluster Name", "demo")
     domain = st.text_input("Domain Name", "cluster.dev")
 
-    eks_version = st.selectbox("EKS Version", ["1.26", "1.25", "1.24"])
+    eks_version = st.selectbox("EKS Version", ["1.27", "1.26", "1.25"])
     instance_types = st.multiselect(
         "Instance Types",
         [
@@ -81,7 +81,7 @@ def generate_stack_eks_yaml(project_yaml):
 
     stack_eks_yaml = {
         "name": "cluster",
-        "template": "https://github.com/shalb/cdev-aws-eks?ref=v0.4.0",
+        "template": "https://github.com/shalb/cdev-aws-eks?ref=main",
         "kind": "Stack",
         "backend": "aws-backend",
         "cliVersion": ">= 0.7.14",
@@ -115,6 +115,29 @@ def generate_stack_eks_yaml(project_yaml):
             },
         }
     }
+    eks_addons_list = [
+        ("ArgoCD", True),
+        ("NGINX", True),
+        ("External Secrets", True),
+        ("Cluster Autoscaler", True),
+        ("AWS LB Controller", True),
+        ("External DNS", True),
+        ("Cert Manager", True),
+        ("EFS", False),
+        ("Cert Manager HTTP Issuers", False),
+        ("Metrics Server", True),
+        ("Reloader", True)
+    ]
+    eks_addons_options = {f"enable_{name.lower().replace(' ', '_')}": state for name, state in eks_addons_list}
+
+    selected_addons = st.multiselect(
+        "Select EKS Addons",
+        options=[name for name, _ in eks_addons_list],
+        default=[name for name, state in eks_addons_list if state]
+    )
+    eks_addons = {f"enable_{name.lower().replace(' ', '_')}": (name in selected_addons) for name, _ in eks_addons_list}
+    stack_eks_yaml["variables"]["eks_addons"] = eks_addons
+
     if not create_vpc:
         stack_eks_yaml["variables"].update(vpc_config)
 
@@ -185,7 +208,7 @@ def create_branch(repo, new_branch_name, base_branch, token):
     r = requests.get(url, headers=headers)
     if r.status_code != 200:
         print(f"Failed to get base branch ref. Response: {r.text}")
-        return False
+        return False, r.json()
 
     ref_sha = r.json().get("object").get("sha")
     data = {
@@ -194,7 +217,8 @@ def create_branch(repo, new_branch_name, base_branch, token):
     }
     url = f"https://api.github.com/repos/{repo}/git/refs"
     r = requests.post(url, json=data, headers=headers)
-    return r.status_code == 201
+    success = r.status_code in [200, 201]
+    return success, r.json() if not success else None
 
 def push_multiple_files_to_github(files, repo, branch, token):
     base_url = f"https://api.github.com/repos/{repo}"
@@ -202,10 +226,14 @@ def push_multiple_files_to_github(files, repo, branch, token):
 
     # Step 1: Get the latest commit SHA of the branch
     r = requests.get(f"{base_url}/git/ref/heads/{branch}", headers=headers)
+    if r.status_code != 200:
+        return False, r.json()
     latest_commit_sha = r.json()["object"]["sha"]
 
     # Step 2: Get the tree SHA of the latest commit
     r = requests.get(f"{base_url}/git/commits/{latest_commit_sha}", headers=headers)
+    if r.status_code != 200:
+        return False, r.json()
     base_tree_sha = r.json()["tree"]["sha"]
 
     # Step 3: Create a new tree object
@@ -218,6 +246,8 @@ def push_multiple_files_to_github(files, repo, branch, token):
         "tree": tree_data
     }
     r = requests.post(f"{base_url}/git/trees", json=tree_payload, headers=headers)
+    if r.status_code != 201:
+        return False, r.json()
     new_tree_sha = r.json()["sha"]
 
     # Step 4: Create a new commit object
@@ -227,6 +257,8 @@ def push_multiple_files_to_github(files, repo, branch, token):
         "parents": [latest_commit_sha]
     }
     r = requests.post(f"{base_url}/git/commits", json=commit_payload, headers=headers)
+    if r.status_code != 201:
+        return False, r.json()
     new_commit_sha = r.json()["sha"]
 
     # Step 5: Update the reference
@@ -234,7 +266,9 @@ def push_multiple_files_to_github(files, repo, branch, token):
         "sha": new_commit_sha
     }
     r = requests.patch(f"{base_url}/git/refs/heads/{branch}", json=ref_payload, headers=headers)
-    return r.status_code in [200, 201]
+    success = r.status_code in [200, 201]
+    # Return both the success status and the response content (if not successful)
+    return success, r.json() if not success else None
 
 def create_pull_request(repo, new_branch_name, base_branch, token):
     url = f"https://api.github.com/repos/{repo}/pulls"
@@ -256,8 +290,9 @@ def create_or_update_github_secret(repo_name, secret_name, secret_value, token):
     headers = {"Authorization": f"token {token}", "Accept": "application/vnd.github+json"}
     response = requests.get(url, headers=headers)
     if response.status_code != 200:
-        print(f"Failed to get public key. Status Code: {response.status_code}. Response: {response.text}")
-        return False
+        error_message = f"Failed to get public key. GitHub says: {response.json().get('message', 'Unknown error')}"
+        print(error_message)
+        return False, error_message
 
     public_key = response.json()['key']
     key_id = response.json()['key_id']
@@ -273,11 +308,12 @@ def create_or_update_github_secret(repo_name, secret_name, secret_value, token):
     response = requests.put(url, json=data, headers=headers)
 
     if response.status_code not in [201, 204]:
-        print(f"Failed to create/update secret. Status Code: {response.status_code}. Response: {response.text}")
-        return False
+        error_message = f"Failed to create/update secret. GitHub says: {response.json().get('message', 'Unknown error')}"
+        print(error_message)
+        return False, error_message
 
     print(f"Secret {secret_name} successfully created/updated.")
-    return True
+    return True, None
 
 st.set_page_config(page_title="Cluster.dev AWS-EKS Configuration")
 st.title("Cluster.dev AWS-EKS Configuration")
@@ -302,47 +338,59 @@ token = st.text_input("GitHub Token:", type="password", help="https://docs.githu
 aws_access_key_id = st.text_input("AWS Access Key ID:",help="https://docs.aws.amazon.com/keyspaces/latest/devguide/access.credentials.html")
 aws_secret_access_key = st.text_input("AWS Secret Access Key:", type="password")
 
-if st.button('Save AWS Secrets') and aws_access_key_id and aws_secret_access_key:
-    if (create_or_update_github_secret(repo, "AWS_ACCESS_KEY_ID", aws_access_key_id, token) and
-        create_or_update_github_secret(repo, "AWS_SECRET_ACCESS_KEY", aws_secret_access_key, token)):
-        st.success("AWS secrets saved successfully!")
+if st.button('Save AWS Secrets'):
+    if aws_access_key_id and aws_secret_access_key:
+        success1, error_message1 = create_or_update_github_secret(repo, "AWS_ACCESS_KEY_ID", aws_access_key_id, token)
+        success2, error_message2 = create_or_update_github_secret(repo, "AWS_SECRET_ACCESS_KEY", aws_secret_access_key, token)
+        if success1 and success2:
+            st.success("AWS secrets saved successfully!")
+        else:
+            error_messages = [msg for msg in [error_message1, error_message2] if msg]
+            for msg in error_messages:
+                st.error(msg)
     else:
-        st.error("Failed to save AWS secrets.")
-else:
-    st.warning("Please provide both AWS Access Key ID and Secret Access Key.")
+        st.warning("Please provide both AWS Access Key ID and Secret Access Key.")
 
 workflow_file_content = generate_workflow_file(project_yaml_content)
 
 # Add a button in Streamlit to trigger the push
-if st.button('Push to GitHub') and repo and token:
-    base_branch = "main"
-    new_branch_name = f"cluster.dev-{uuid.uuid4().hex[:8]}"
-    project = yaml.safe_load(project_yaml_content)
-    subdirectory = ".cluster.dev/"+project["name"]
-    workflow_path = ".github/workflows/clusterdev-"+project["name"]+".yaml"
+if st.button('Push to GitHub'):
+    if repo and token:
+        base_branch = "main"
+        new_branch_name = f"cluster.dev-{uuid.uuid4().hex[:8]}"
+        project = yaml.safe_load(project_yaml_content)
+        subdirectory = ".cluster.dev/"+project["name"]
+        workflow_path = ".github/workflows/clusterdev-"+project["name"]+".yaml"
 
-    files_to_commit = {
-        f"{subdirectory}/project.yaml": project_yaml_content,
-        f"{subdirectory}/backend.yaml": backend_yaml_content,
-        f"{subdirectory}/stack_eks.yaml": stack_eks_yaml_content,
-        workflow_path: workflow_file_content
-    }
+        files_to_commit = {
+            f"{subdirectory}/project.yaml": project_yaml_content,
+            f"{subdirectory}/backend.yaml": backend_yaml_content,
+            f"{subdirectory}/stack_eks.yaml": stack_eks_yaml_content,
+            workflow_path: workflow_file_content
+        }
 
-    # Create a new branch
-    if not create_branch(repo, new_branch_name, base_branch, token):
-        st.error("Failed to create branch.")
-
-    # Push the files to the new branch
-    if push_multiple_files_to_github(files_to_commit, repo, new_branch_name, token):
-        # Create a pull request
-        pr_url = create_pull_request(repo, new_branch_name, base_branch, token)
-        if pr_url:
-            st.success(f"Files pushed successfully! [View Pull Request]({pr_url})")
+        # Create a new branch
+        branch_success, branch_error_response = create_branch(repo, new_branch_name, base_branch, token)
+        if not branch_success:
+            if branch_error_response and "message" in branch_error_response:
+                st.error(f"Failed to create branch. GitHub says: {branch_error_response['message']}")
+            else:
+                st.error("Failed to create branch.")
         else:
-            st.error("Failed to create pull request.")
+            # Push the files to the new branch
+            push_success, push_error_response = push_multiple_files_to_github(files_to_commit, repo, new_branch_name, token)
+
+            if push_success:
+                # Create a pull request
+                pr_url = create_pull_request(repo, new_branch_name, base_branch, token)
+                if pr_url:
+                    st.success(f"Files pushed successfully! [View Pull Request]({pr_url})")
+            else:
+                if push_error_response and "message" in push_error_response:
+                    st.error(f"Failed to push files. GitHub says: {push_error_response['message']}")
+                else:
+                    st.error("Failed to push files.")
     else:
-        st.error("Failed to push files.")
-else:
-    st.warning("Please provide both repository and token.")
+        st.warning("Please provide both repository and token.")
 
 
